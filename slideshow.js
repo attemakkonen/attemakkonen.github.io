827433a46a0137f6a839671dd2df8cea37b39abf
Atte Makkonen <atte.makkonen@aalto.fi>
2017-03-20 10:50:41 +0200

    videot

--- slideshow.js
+++ slideshow.js
@@ -1,102 +1,67 @@
-var current = 0;
-var laskuri = 2;
+var index = 0;
 
-function asetaCurrent() {
-  if ((localStorage.getItem('index') === undefined) || (localStorage.getItem('index') === null) || (localStorage.getItem('index') > laskuri)) {
-    localStorage.setItem('index', 0);
-    current = 0;
-  } else {
-    current = parseInt(localStorage.getItem('index'));
-  }
-}
-
-window.onload = function() {
-    
-  console.log(current);
-  setcurrent();
-  console.log(current);  $.getJSON("https://makkona4.firebaseio.com/uutiset.json", function(data) {
-    console.log(data);
-    $('#otsikko').html(data[current].otsikko);
-    $('#päivämäärä').html(data[current].päivämäärä);
-    $('#teksti').html(data[current].sisältö);
-    return data;
-});
-};
-
-function animaatio() {
-  $('.efekti').fadeOut('normal', function(){
-    jiison();
-  }).fadeIn("normal");
-}
-
-function jiison() {
-        $.getJSON("https://makkona4.firebaseio.com/uutiset.json", function (data) {
-
-          $('#otsikko').html(data[current].otsikko);
-          $('#päivämäärä').html(data[current].sisältö);
-          $('#teksti').html(data[current].päivämäärä);
-
-      //
-      return data;
+window.onload = function () {
+    'use strict';
+    $.getJSON("https://makkona4.firebaseio.com/uutiset.json", function (data) {
+        console.log(data);
+        index = parseInt(localStorage.getItem('indeksi'));
+        display();
     });
-}
+};
 
 function display() {
     'use strict';
     $.getJSON("https://makkona4.firebaseio.com/uutiset.json", function (data) {
-        $('#otsikko').hide().html(data.uutiset[index].otsikko).fadeIn(3000);
-        $('#paivamaara').hide().html(data.uutiset[index].paivamaara).fadeIn(3000);
-        $('#teksti').hide().html(data.uutiset[index].sisalto).fadeIn(3000);
-        
-        localStorage.setItem("current", index);
+        $('#otsikko').hide().html(data.uutiset[index].otsikko).fadeIn(2000);
+        $('#päivämäärä').hide().html(data.uutiset[index].päivämäärä).fadeIn(2000);
+        $('#sisältö').hide().html(data.uutiset[index].sisältö).fadeIn(2000);
+        localStorage.setItem("indeksi", index);
     });
 }
 
-function seuraavaSlide() {
+function nextSlide() {
     'use strict';
-    if(current < 2) {
-        current += 1;
-        display();
-    } else {
-        current = 0;
-        display();
-        console.log(current);
-       
-    }
-}
-
-function edellinenSlide() {
-    'use strict'
-    if(current < 2) {
-        current += 1;
+    if (index < 2) {
+        index += 1;
         display();
     } else {
         index = 0;
         display();
     }
-
 }
 
-var pysäytetty = false;
+function previousSlide() {
+    'use strict';
+    if (index > 0) {
+        index -= 1;
+        display();
+    } else {
+        index = 2;
+        display();
+    }
+}
 
-var aika = setInterval(function () { seuraavaSlide(); }, 5000);
+var isPaused = false;
+
+var myVar = setInterval(function () { nextSlide(); }, 10000);
 
 function playpause() {
     'use strict';
-    if (!pysäytetty) {    
+    if (!isPaused) {    
         window.clearInterval(myVar);
-        pysäytetty = true;
+        isPaused = true;
     } else {
-        valinta = setInterval(function () { seuraavaSlide(); }, 5000);
-        pysäytetty = false;
+        myVar = setInterval(function () { nextSlide(); }, 10000);
+        isPaused = false;
     }
 }
 
-function toggleText(painike) {
+function toggleText(button_id) {
     'use strict';
-    if (pysäytetty) {
-        document.getElementById(painike).textContent = "play";
+    if (isPaused) {
+        document.getElementById(button_id).textContent = "play";
     } else {
-        document.getElementById(painike).textContent = "pause";
+        document.getElementById(button_id).textContent = "pause";
     }
 }
+
